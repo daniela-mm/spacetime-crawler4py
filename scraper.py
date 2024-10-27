1fe87deeb55fa5fbba3592d43fad58b4b08971e0
Daniela Mendoza Martinez <dmendo15@circinus-47.ics.uci.edu>
2024-10-27 02:43:14 -0700

    Basic Functionality

--- scraper.py
+++ scraper.py
@@ -1,11 +1,16 @@
 import re
 from urllib.parse import urlparse
+from bs4 import BeautifulSoup
+
 
 def scraper(url, resp):
+    print("In scaper function.")
     links = extract_next_links(url, resp)
     return [link for link in links if is_valid(link)]
 
 def extract_next_links(url, resp):
+    #Create an empty list to hold all the VALID urls in. 
+    url_list = list()
     # Implementation required.
     # url: the URL that was used to get the page
     # resp.url: the actual url of the page
@@ -14,17 +19,36 @@ def extract_next_links(url, resp):
     # resp.raw_response: this is where the page actually is. More specifically, the raw_response has two parts:
     #         resp.raw_response.url: the url, again
     #         resp.raw_response.content: the content of the page!
+
+    #Case 1: If raw-response if NOT empty (ie status code is 200): 
+    if(resp.status == 200):
+        # Find all the links in the page
+        soup = BeautifulSoup(resp.raw_response.content, 'html.parser')
+        # Print out all the links found. 
+        for link in soup.find_all('a'):
+            print(link.get('href'))
+            #Append the link to the list of links. 
+            url_list.append(link)
+    #Case 2: If raw response IS empty:
     # Return a list with the hyperlinks (as strings) scrapped from resp.raw_response.content
-    return list()
+    return url_list
 
 def is_valid(url):
+    valid_domains = [".ics.uci.edu/", ".cs.uci.edu/", "informatics.uci.edu", ".stat.uci.edu", "today.uci.edu/department/information_computer_sciences"]
     # Decide whether to crawl this url or not. 
     # If you decide to crawl it, return True; otherwise return False.
     # There are already some conditions that return False.
     try:
         parsed = urlparse(url)
+        #Defragment the URL before further processing: 
+        parsed = parsed.split('#')[0]
         if parsed.scheme not in set(["http", "https"]):
             return False
+        #Check that the urls are within the domain and paths.
+        if (parsed.netloc not in  valid_domains):
+            print(f"{parsed} URL is NOT in the domain. ")
+            return False
+        #Check if there is HIGH text content;
         return not re.match(
             r".*\.(css|js|bmp|gif|jpe?g|ico"
             + r"|png|tiff?|mid|mp2|mp3|mp4"
